49d2e7b5bcb7216b9832778f9c94f1f1c84a10e3
arvel <franciscoarvel123@gmail.com>
2023-10-03 09:43:29 +0800

    fourth commit

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
-const MySQLEvents = require("@rodrigogs/mysql-events");
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://bias-server-1.onrender.com/",
+    origin: "http://192.168.56.1:3000",
     methods: ["GET", "POST"],
   },
 });
